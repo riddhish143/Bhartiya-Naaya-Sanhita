9c56fbb554c6a8dcd0e700c971f43863038005b2
riddhish143 <riddhishmahajan143@gmail.com>
2024-09-19 21:39:17 +0530

    chapter10 added

---
+++ Chapter10/chapter10.js
@@ -0,0 +1,65 @@
+import { addChapter, saveJSON } from "../Template/template.js"
+import fs from "fs";
+
+addChapter("Counterfeiting coin, Government stamps, currency-notes or bank-notes.",
+  `Whoever counterfeits, or knowingly performs any part of the process of counterfeiting, any coin, stamp issued by Government for the purpose of revenue, currency-note or bank-note, shall be punished with imprisonment for life, or with imprisonment of either description for a term which may extend to ten years, and shall also be liable to fine.
+Explanation.—For the purposes of this Chapter,—
+(1) the expression “bank-note” means a promissory note or engagement for the payment of money to bearer on demand issued by any person carrying on the business of banking in any part of the world, or issued by or under the authority of any State or Sovereign Power, and intended to be used as equivalent to, or as a substitute for money;
+(2) “coin” shall have the same meaning as assigned to it in section 2 of the Coinage Act, 2011 (11 of 2011) and includes metal used for the time being as money and is stamped and issued by or under the authority of any State or Sovereign Power intended to be so used;
+(3) a person commits the offence of “counterfeiting Government stamp” who counterfeits by causing a genuine stamp of one denomination to appear like a genuine stamp of a different denomination; (4) a person commits the offence of counterfeiting coin who intending to practise deception, or knowing it to be likely that deception will thereby be practised, causes a genuine coin to appear like a different coin; and
+(5) the offence of “counterfeiting coin” includes diminishing the weight or alteration of the composition, or alteration of the appearance of the coin.`
+)
+
+addChapter("Using as genuine, forged or counterfeit coin, Government stamp, currency-notes or bank-notes.",
+  `Whoever imports or exports, or sells or delivers to, or buys or receives from, any other person, or otherwise traffics or uses as genuine, any forged or counterfeit coin, stamp, currency-note or bank-note, knowing or having reason to believe the same to be forged or counterfeit, shall be punished with imprisonment for life, or with imprisonment of either description for a term which may extend to ten years, and shall also be liable to fine.`
+)
+
+addChapter("Possession of forged or counterfeit coin, Government stamp, currency-notes or bank-notes.",
+  `Whoever has in his possession any forged or counterfeit coin, stamp, currency-note or bank-note, knowing or having reason to believe the same to be forged or counterfeit and intending to use the same as genuine or that it may be used as genuine, shall be punished with imprisonment of either description for a term which may extend to seven years, or with fine, or with both.
+Explanation.—If a person establishes the possession of the forged or counterfeit coin, stamp, currency-note or bank-note to be from a lawful source, it shall not constitute an offence under this section.`
+)
+
+addChapter("Making or possessing instruments or materials for forging or counterfeiting coin, Government stamp, currency-notes or bank-notes.",
+  `Whoever makes or mends, or performs any part of the process of making or mending, or buys or sells or disposes of, or has in his possession, any machinery, die, or instrument or material for the purpose of being used, or knowing or having reason to believe that it is intended to be used, for forging or counterfeiting any coin, stamp issued by Government for the purpose of revenue, currency-note or bank-note, shall be punished with imprisonment for life, or with imprisonment of either description for a term which may extend to ten years, and shall also be liable to fine.`
+)
+
+addChapter("Making or using documents resembling currency-notes or bank-notes.",
+  `(1) Whoever makes, or causes to be made, or uses for any purpose whatsoever, or delivers to any person, any document purporting to be, or in any way resembling, or so nearly resembling as to be calculated to deceive, any currency-note or bank-note shall be punished with fine which may extend to three hundred rupees.
+(2) If any person, whose name appears on a document the making of which is an offence under sub-section (1), refuses, without lawful excuse, to disclose to a police officer on being so required the name and address of the person by whom it was printed or otherwise made, he shall be punished with fine which may extend to six hundred rupees.
+(3) Where the name of any person appears on any document in respect of which any person is charged with an offence under sub-section (1) or on any other document used or distributed in connection with that document it may, until the contrary is proved, be presumed that the person caused the document to be made.`
+)
+
+addChapter("Effacing writing from substance bearing Government stamp, or removing from document a stamp used for it, with intent to cause loss to Government.",
+  `Whoever, fraudulently or with intent to cause loss to the Government, removes or effaces from any substance, bearing any stamp issued by Government for the purpose of revenue, any writing or document for which such stamp has been used, or removes from any writing or document a stamp which has been used for such writing or document, in order that such stamp may be used for a different writing or document, shall be punished with imprisonment of either description for a term which may extend to three years, or with fine, or with both`
+)
+
+addChapter("Using Government stamp known to have been before used.",
+  `Whoever, fraudulently or with intent to cause loss to the Government, uses for any purpose a stamp issued by Government for the purpose of revenue, which he knows to have been before used, shall be punished with imprisonment of either description for a term which may extend to two years, or with fine, or with both`
+)
+
+addChapter("Erasure of mark denoting that stamp has been used.",
+  `Whoever, fraudulently or with intent to cause loss to Government, erases or removes from a stamp issued by Government for the purpose of revenue, any mark, put or impressed upon such stamp for the purpose of denoting that the same has been used, or knowingly has in his possession or sells or disposes of any such stamp from which such mark has been erased or removed, or sells or disposes of any such stamp which he knows to have been used, shall be punished with imprisonment of either description for a term which may extend to three years, or with fine, or with both.
+`
+)
+
+addChapter("Prohibition of fictitious stamps.",
+  `(1) Whoever—.
+(a) makes, knowingly utters, deals in or sells any fictitious stamp, or knowingly uses for any postal purpose any fictitious stamp; or.
+(b) has in his possession, without lawful excuse, any fictitious stamp; or.
+(c) makes or, without lawful excuse, has in his possession any die, plate, instrument or materials for making any fictitious stamp, shall be punished with fine which may extend to two hundred rupees..
+(2) Any such stamp, die, plate, instrument or materials in the possession of any person for making any fictitious stamp may be seized and, if seized shall be forfeited..
+(3) In this section “fictitious stamp” means any stamp falsely purporting to be issued by Government for the purpose of denoting a rate of postage, or any facsimile or imitation or representation, whether on paper or otherwise, of any stamp issued by Government for that purpose.
+(4) In this section and also in sections 178 to 181 (both inclusive), and sections 183 to 185 (both inclusive) the word “Government”, when used in connection with, or in reference to any stamp issued for the purpose of denoting a rate of postage, shall, notwithstanding anything in clause (12) of section 2, be deemed to include the person or persons authorised by law to administer executive Government in any part of India or in any foreign country.`
+)
+
+addChapter("Person employed in mint causing coin to be of different weight or composition from that fixed by law.",
+  `Whoever, being employed in any mint lawfully established in India, does any act, or omits what he is legally bound to do, with the intention of causing any coin issued from that mint to be of a different weight or composition from the weight or composition fixed by law, shall be punished with imprisonment of either description for a term which may extend to seven years, and shall also be liable to fine.
+`
+)
+
+addChapter("Unlawfully taking coining instrument from mint.",
+  `Whoever, without lawful authority, takes out of any mint, lawfully established in India, any coining tool or instrument, shall be punished with imprisonment of either description for a term which may extend to seven years, and shall also be liable to fine.
+`
+)
+
+saveJSON('chapter10.json')
